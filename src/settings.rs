10614113c8937ee00a944eb595251ccfd71b5c96
Noah Rosenzweig <noah@aware.vision>
2019-01-19 02:52:30 +0100

    Added levels and images

--- src/settings.rs
+++ src/settings.rs
@@ -110,7 +110,7 @@ pub mod level {
 pub mod level_manager {
   pub const AUDIO_FORMAT: &str = "ogg";
   pub const LEVEL_NAMES: [&'static str; 10] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-  pub const SONG_NAMES:  [&'static str; 10] = ["ingame_1", "ingame_2", "ingame_2.5", "ingame_3", "ingame_1", "ingame_2", "ingame_2.5", "ingame_3", "ingame_1", "ingame_2"];
+  pub const SONG_NAMES:  [&'static str; 10] = ["ingame_1", "ingame_2.5", "ingame_3", "ingame_2", "ingame_1", "ingame_2", "ingame_2.5", "ingame_3", "ingame_1", "ingame_2"];
 
 
 
