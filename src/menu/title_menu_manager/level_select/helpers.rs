d6d7ad26b7dcd12a23a5ceeef828a8013af32b1f
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-02-25 06:59:01 +0100

    Rewrote level select buttons generation

--- src/menu/title_menu_manager/level_select/helpers.rs
+++ src/menu/title_menu_manager/level_select/helpers.rs
@@ -4,11 +4,11 @@ use ggez::{
   graphics::{ Text, Font },
 };
 use noframe::geo::prelude::*;
+use noframe::geo::mask::misc::Side;
 
-use settings::res::{ MISSING_IMAGE, fonts };
+use settings::res::fonts;
 use settings::buttons;
 use settings::level_manager::LEVEL_NAMES;
-use settings::menus;
 use settings::menus::title::level_select::*;
 use menu::prelude::*;
 
@@ -21,50 +21,63 @@ pub fn new_animation(ctx: &mut Context, _window_size: &Size) -> Animation {
 }
 
 pub fn new_buttons(ctx: &mut Context, window_size: &Size) -> GameResult<Vec<Button>> {
-  let columns = 3.0;
-  let window_rect = Rect::new(Point::new(0.0, 0.0), window_size.clone(), Origin::TopLeft);
-  let padding          = 32.0;  //8.0;
-  let border_padding   = 32.0;
-  let size             = Size::new((window_size.w - border_padding * 2.0 - padding * (columns - 1.0)) / (columns), 80.0, /*72.0*/ /*32.0*/);
-  let back_btn_size    = Size::new(64.0, 64.0);
-  let initial_top_left = Point::new(border_padding, border_padding);
-  let font             = Font::new_px(ctx, fonts::DEFAULT, FONT_SIZE)?;
-  let text_offset      = Point::new(/*padding*/ 144.0, size.h * 0.5);
-  let entries_per_col  = ((window_size.h - back_btn_size.h - border_padding * 3.0) / (size.h + padding)) as usize;
+  let columns = 3;
+  let rows    = 5;
+  let padding          = Point::new(48.0, 32.0);
+  let border_padding   = Point::new(64.0, 32.0);
+  let buttons_area     = Rect::new(
+    Point::new(border_padding.x, border_padding.y),
+    Size::new(
+      window_size.w - border_padding.x * 2.0,
+      window_size.h - border_padding.y * 2.0
+    ),
+    Origin::TopLeft
+  );
+  let size             = Size::new(
+    (buttons_area.size().w - padding.x * (columns - 1) as NumType) / columns as NumType,
+    80.0
+  );
+  let column_point = Point::new(
+    buttons_area.side(Side::Left) + size.w / 2.0,
+    buttons_area.side(Side::Top)
+  );
+  let text_offset  = Point::new(-80.0, 0.0);
+  let font         = Font::new_px(ctx, fonts::DEFAULT, FONT_SIZE)?;
+  let mut buttons  = Vec::new();
 
-  let back_button = ButtonBuilder::new(ctx)
-    .point(window_rect.bottom_left() + Point::new(border_padding, -border_padding))
-    .size(back_btn_size)
-    .origin(Origin::BottomLeft)
-    .button_type(ButtonType::LevelSelectBack)
-    .animation_from(vec![::join_str(buttons::IMAGES, "return.png")], vec![1000])
-    .build().expect("Should build LevelSelectBack Button");
+  for col in 0 .. columns {
+    for row in 0 .. rows {
+      let i    = rows * col + row;
+      let name = LEVEL_NAMES[i];
+      let image_filename = &format!("level_select{}.png", col + 1);
+      let center = Point::new(
+        column_point.x + (size.w + padding.x) * col as NumType,
+        column_point.y + size.h / 2.0 + (size.h + padding.y) * row as NumType
+      );
+      let origin = Origin::Center;
+      let text   = Text::new(ctx, &format!("{}) {}", i + 1, &::semantic(&name.replace(".json", ""))), &font)?;
+      buttons.push(ButtonBuilder::new(ctx)
+                   .point(center.clone())
+                   .size(size.clone())
+                   .origin(origin)
+                   .button_type(ButtonType::LevelSelectLevel(i))
+                   .animation_from(vec![::join_str(buttons::IMAGES, image_filename)], vec![1000])
+                   .text_from(
+                     center + text_offset.clone(),
+                     Size::new(text.width() as NumType, text.height() as NumType),
+                     Origin::CenterLeft,
+                     text
+                   ).build()?);
+    }
+  }
 
-  Ok(LEVEL_NAMES.iter().enumerate()
-     .map( |(i, name)| {
-       let col = i / entries_per_col;
-       let top_left = initial_top_left.clone() +
-         Point::new((size.w + padding) * col as NumType,
-         (size.h + padding) * (i - col * entries_per_col) as NumType);
-       let text = Text::new(ctx, &format!("{}) {}", i + 1, &::semantic(&name.replace(".json", ""))), &font)?;
-       let mut image_filename_n = col + 1;
-       if image_filename_n < 1 || image_filename_n > 3 {
-         image_filename_n = 1;
-       }
-       let image_filename = &format!("level_select{}.png", image_filename_n);
-       Ok(ButtonBuilder::new(ctx)
-          .point(top_left.clone())
-          .size(size.clone())
-          .origin(Origin::TopLeft)
-          .button_type(ButtonType::LevelSelectLevel(i))
-          .animation_from(vec![::join_str(buttons::IMAGES, image_filename)], vec![1000])
-          .text_from(
-            top_left + text_offset.clone(),
-            Size::new(text.width() as NumType, text.height() as NumType),
-            Origin::CenterLeft,
-            text
-          ).build().expect("Should build LevelSelectLevel Button")
-       )
-     })
-  .chain(vec![Ok(back_button)]).collect::<GameResult<Vec<Button>>>()?)
+  buttons.push(ButtonBuilder::new(ctx)
+               .point(buttons_area.bottom_left())
+               .size(Size::new(64.0, 64.0))
+               .origin(Origin::BottomLeft)
+               .button_type(ButtonType::LevelSelectBack)
+               .animation_from(vec![::join_str(buttons::IMAGES, "return.png")], vec![1000])
+               .build()?);
+
+  Ok(buttons)
 }
